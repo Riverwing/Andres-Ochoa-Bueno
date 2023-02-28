bbddfa4f5134338311b39456cec7c4fac678398f
Riverwing <saintandrews88@hotmail.com>
2023-02-28 14:57:01 +0100

    consigo configurar config de tailwind para aplicar algunos colores de bs, comienzo car de ranking

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../*.{html,js}"],
+  content: ["../*.{html,js}", './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        bootstrap: {
+          gray: '#212529', /* para el header y el footer */
+          info:'#54B4D3',
+        }
+      }
+
+    },
   },
   plugins: [],
 }
