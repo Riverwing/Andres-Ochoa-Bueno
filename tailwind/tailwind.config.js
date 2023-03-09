a8b0002299b181749e920ce3c7bb9a4e59d7e76e
Riverwing <saintandrews88@hotmail.com>
2023-03-09 01:32:23 +0100

    sigo mejorando el header de tailwind

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../*.{html,js}", './src/**/*.{js,jsx,ts,tsx}'],
+  content: ["../*.{html,js}", "./src/**/*.{js,jsx,ts,tsx}", "./src/**/*.{html,js}"],
   theme: {
     extend: {
 
@@ -9,8 +9,19 @@ module.exports = {
         current: 'currentColor',
         bootstrap: {
           gray: '#212529', /* para el header y el footer */
-          info:'#54B4D3',
+          info: '#54B4D3',
+          letra: '#8A8C8E'
         }
+      },
+
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '4rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
       }
 
     },
